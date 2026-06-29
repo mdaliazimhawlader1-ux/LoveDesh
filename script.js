b7d5ef9e6eac55449cf05c680d39ee5c36a50799
mdaliazimhawlader1-ux <mdaliazimhawlader1@gmail.com>
2026-06-29 11:55:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const button = document.querySelector("button");
+const container = document.querySelector(".container");
 
+button.onclick = function () {
+  let name = prompt("তোমার নাম লিখো:");
+  let post = prompt("কি পোস্ট করতে চাও?");
+
+  if (name && post) {
+    container.innerHTML += `
+      <div class="post">
+        <h3>${name}</h3>
+        <p>${post}</p>
+      </div>
+    `;
+  }
+};
